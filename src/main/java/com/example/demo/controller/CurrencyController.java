a118fddca69a21c65f4e8cd9fa63e4ecde004151
nentoday <nastiabogach@ukr.net>
2023-05-24 19:23:04 +0300

    updated controller

--- src/main/java/com/example/demo/controller/CurrencyController.java
+++ src/main/java/com/example/demo/controller/CurrencyController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -39,40 +40,19 @@ public class CurrencyController {
         currencyService.saveCurrency(currency);
         return "redirect:/currency";
     }
+    @GetMapping("/currency/{currencyId}/edit")
+    public String editCurrencyForm(@PathVariable("currencyId") Long currencyId,Model model){
+        CurrencyDto currency=currencyService.findByClubId(currencyId);
+        model.addAttribute("currency", currency);
+        return "currency-edit";
+
+    }
+    @PostMapping("/currency/{currencyId}/edit")
+    public String updateCurrency(@PathVariable("currencyId") Long currencyId, @ModelAttribute("currency") CurrencyDto currency){
+        currency.setId(currencyId);
+        currencyService.updateCurrency(currency);
+        return"redirect:/currency";
+    }
+
+
 }
-
-
-//import com.example.demo.repository.CurrencyRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.ui.Model;
-//import java.util.List;
-//
-//import java.util.Currency;
-//
-//
-//@Controller
-//public class CurrencyController {
-//    @Autowired
-//    private CurrencyRepository currencyRepository;
-//    @GetMapping("/")
-//    public String showcurrency(Model model){
-//        List <Currency> currencies = currencyRepository.findDistinctCurrency();
-//        return "home";
-//    }
-//    @PostMapping("/")
-//    public String GetCurrencyData(
-//        @RequestParam String currency,
-//        @RequestParam String dayFrom,
-//        @RequestParam String dayTo,
-//        Model model){
-//        // Обробка запиту та передача даних на сторінку
-//        model.addAttribute("currency", currency);
-//        model.addAttribute("dayFrom", dayFrom);
-//        model.addAttribute("dayTo", dayTo);
-//        return "home";
-//    }
-//}
