a7a56813aa7078bc447cd15e51e26ce4c71df0ef
nentoday <nastiabogach@ukr.net>
2023-05-26 22:26:20 +0300

    added date repository

--- src/main/java/com/example/demo/repository/DateRepository.java
+++ src/main/java/com/example/demo/repository/DateRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface dateRepository extends JpaRepository<Date, Long> {
+public interface DateRepository extends JpaRepository<Date, Long> {
 }
